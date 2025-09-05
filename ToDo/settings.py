4001d6ba32e66dbc3f37aca8e360d5b161314bcb
Jennifer Eze <ezehjennifa@gmail.com>
2025-09-05 12:12:44 +0100

    update

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
@@ -130,4 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = '/users/login/'
 LOGOUT_REDIRECT_URL = 'login'
